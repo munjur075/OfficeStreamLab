42b6e120b6d9943465e3a539d5a303493c5178fb
munjur075 <munjuralom.jvai@gmail.com>
2025-09-08 18:08:01 +0600

    Films Partial Done for admin panel

--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserManagementView
+from .views import UserManagementView, AdminFilmsView
 
 urlpatterns = [
     path('manage-users', UserManagementView.as_view(), name='manage_users'),
+    path('films', AdminFilmsView.as_view(), name='films'),
 ]
--- adminpanel/views.py
+++ adminpanel/views.py
@@ -2,9 +2,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser
 from rest_framework import status
-from django.db.models import Q
+from django.db.models import Q, Sum
 
 from accounts.models import User
+from movie.models import Film
 from .serializers import ManageUserSerializer
 
 
@@ -58,3 +59,83 @@ class UserManagementView(APIView):
                 "status": "error",
                 "message": "User not found"
             }, status=status.HTTP_404_NOT_FOUND)
+        
+
+
+# =========================== Films ============================
+class AdminFilmsView(APIView):
+    permission_classes = [IsAdminUser]  # Only admins can access
+
+    def get(self, request):
+        films = Film.objects.all()
+
+        stats = {
+            "total_films": films.count(),
+            "total_buy": films.aggregate(total=Sum("total_buy_earning"))["total"] or 0,
+            "total_rent": films.aggregate(total=Sum("total_rent_earning"))["total"] or 0,
+        }
+
+        # Get only under review films
+        under_review_films = Film.objects.filter(status="REVIEW")
+        review_films = [
+            {
+                "id": f.id,
+                # "filmmaker": str(f.filmmaker),
+                "title": f.title,
+                "film_type": f.get_film_type_display(),
+                "thumbnail": f.thumbnail.url if f.thumbnail else None,
+                "release_date": f.created_at.date(),
+            }
+            for f in under_review_films
+        ]
+
+        # Filter films where status is either "PUBLISHED" or "REJECTED"
+        track_all_films = Film.objects.filter(status__in=["PUBLISHED", "REJECTED"])
+
+        track_films = [
+            {
+                "id": f.id,
+                "filmmaker": str(f.filmmaker),
+                "title": f.title,
+                "film_type": f.get_film_type_display(),
+                "status": f.get_status_display(),
+                "release_date": f.created_at.date(),
+                "total_views": f.total_views,
+                "total_earning": f.total_earning
+            }
+            for f in track_all_films
+        ]
+
+        return Response({
+            "status": "success",
+            "message": "Films fetched successfully",
+            "stats": stats,
+            "review_films": review_films,
+            "track_all_films": track_films,
+        })
+
+
+    # # DELETE: remove a user by ID (only non-admin users)
+    # def delete(self, request):
+    #     user_id_param = request.GET.get("user_id", "").strip()
+
+    #     # user_id_param = request.data.get("user_id").strip()
+    #     if not user_id_param:
+    #         return Response({
+    #             "status": "error",
+    #             "message": "User ID is required"
+    #         }, status=status.HTTP_400_BAD_REQUEST)
+
+    #     try:
+    #         # Exclude admin to prevent accidental deletion
+    #         user = User.objects.exclude(role="admin").get(id=user_id_param)
+    #         user.delete()
+    #         return Response({
+    #             "status": "success",
+    #             "message": f"User {user.full_name} deleted successfully"
+    #         })
+    #     except User.DoesNotExist:
+    #         return Response({
+    #             "status": "error",
+    #             "message": "User not found"
+    #         }, status=status.HTTP_404_NOT_FOUND)
